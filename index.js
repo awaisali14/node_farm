7d64dd38216dfbb9f5ede93590422bc1e3acf89f
awaisali14 <aawais363@gmail.com>
2021-09-08 17:31:06 +0500

    task-2

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt","utf-8");
+// Blocking, Synchronous way:
 
+// const textIn = fs.readFileSync("./txt/input.txt","utf-8");
 // console.log(textIn);
+// const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()} `;
+// fs.writeFileSync("./txt/Output.txt", textOut);
+// console.log(textOut);
 
-const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now()} `;
-fs.writeFileSync("./txt/Output.txt", textOut);
-console.log(textOut);
+// Non-Blocking, Asynchronous way:
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    console.log(data1);
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        console.log(data2);
+      });
+      
+  });
+  
+
+console.log("This has to run before.");
